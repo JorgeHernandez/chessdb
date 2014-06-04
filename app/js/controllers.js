4fa13ff4dc583e37907f5baf10f48f18f61b700c
Jorge Hernandez <jorge.hernandez64@outlook.es>
2014-06-04 05:53:38 -0400

    http json data access

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,29 +1,9 @@
 var chessdbApp = angular.module('chessdbApp', []);
 
-chessdbApp.controller('GameListCtrl', function ($scope) {
-  $scope.games = [{
-    "id":1,
-    "date":"01/01/1970",
-    "white":"Karpov",
-    "black":"Kasparov",
-    "opening":"Sicilian Defense",
-    "result":"white",
-    "synopsis":"very boring match...."
-},{
-    "id":2,
-    "date":"01/01/1970",
-    "white":"Karpov",
-    "black":"Kasparov",
-    "opening":"Sicilian Defense",
-    "result":"white",
-    "synopsis":"very boring match...."
-},{
-    "id":1,
-    "date":"01/01/1970",
-    "white":"Karpov",
-    "black":"Kasparov",
-    "opening":"Sicilian Defense",
-    "result":"white",
-    "synopsis":"very boring match...."
-}];
+chessdbApp.controller('GameListCtrl', function ($scope, $http) {
+  $http.get('data/games.json').success(function(data) {
+    $scope.games = data;
+  });
+$scope.gameOrder = 'id';
+
 });
