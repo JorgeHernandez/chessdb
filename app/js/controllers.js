07365c6fac87f53604f662cd74bd556af71b2a33
Jorge Hernandez <jorge.hernandez64@outlook.es>
2014-06-04 06:55:49 -0400

    routing - layout changed to composite view - master detail

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,14 @@
-var chessdbApp = angular.module('chessdbApp', []);
+var chessdbControllers = angular.module('chessdbControllers', []);
 
-chessdbApp.controller('GameListCtrl', function ($scope, $http) {
-  $http.get('data/games.json').success(function(data) {
-    $scope.games = data;
-  });
-$scope.gameOrder = 'id';
+chessdbControllers.controller('GameListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('data/games.json').success(function(data) {
+      $scope.games = data;
+    });
+    $scope.gameOrder = 'id';
+  }]);
 
-});
+chessdbControllers.controller('GameDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.id = $routeParams.id;
+  }]);
