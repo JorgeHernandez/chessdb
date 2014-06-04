6ede5df4dcb75ef606cda0102ff63ac11bd5a8ee
Jorge Hernandez <jorge.hernandez64@outlook.es>
2014-06-04 07:42:47 -0400

    game detail view

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,24 @@
 var chessdbControllers = angular.module('chessdbControllers', []);
 
-chessdbControllers.controller('GameListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('data/games.json').success(function(data) {
-      $scope.games = data;
-    });
-    $scope.gameOrder = 'id';
-  }]);
+chessdbControllers.controller('GameListCtrl', [
+    '$scope', 
+    '$http',
+    function ($scope, $http) {
+        $http.get('data/games.json').success(function(data) {
+            $scope.games = data;
+        });
+        $scope.gameOrder = 'id';
+    }
+]);
 
-chessdbControllers.controller('GameDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.id = $routeParams.id;
-  }]);
+chessdbControllers.controller('GameDetailCtrl', [
+    '$scope', 
+    '$routeParams', 
+    '$http',
+    function($scope, $routeParams,$http) {
+        $scope.id = $routeParams.id;
+        $http.get('data/game_' + $routeParams.id + '.json').success(function(data) {
+            $scope.movements = data;
+        });
+    }
+]);
